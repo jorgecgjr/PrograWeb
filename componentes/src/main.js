748be53fafdb5c2c3af5da20b2294202513eedfe
Jorge Correa Gutiérrez <jorgecgjr@hotmail.com>
2025-04-10 10:24:09 -0600

    Clase jueves 10 de abril

--- componentes/src/main.js
+++ componentes/src/main.js
@@ -4,6 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import '@popperjs/core'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 const app = createApp(App)
 
 app.use(router)
